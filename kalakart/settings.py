ee35e69547347ed11462206ce7439d0cc2911f65
Gayathri <gayathri.avisineni@gmail.com>
2026-05-23 17:54:59 +0530

    fix allowed hosts

--- kalakart/settings.py
+++ kalakart/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-o#!n^(6fqv3sf4wza(n7&eznd-dr40-whfa)a1%hd^$9%vtz4i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['kalakart-gpwb.onrender.com',]
 
 
 # Application definition
